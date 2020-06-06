b03472111b89c682560729be15143f0478ee0a46
Adam Wright <39059755+Adamw0615@users.noreply.github.com>
2020-06-06 16:24:11 -0400

    Add files via upload

---
+++ public/public/dist/app.js
@@ -0,0 +1,1 @@
+console.error("Error: Cannot find module 'C:\\Users\\Adam\\Desktop\\browserify\\extracted\\public\\public\\app\\index.js' from 'C:\\Users\\Adam\\Desktop\\browserify\\extracted\\public'");
